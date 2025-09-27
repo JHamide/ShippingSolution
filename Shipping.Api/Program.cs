7c18257980b0a730118b67ae7df524caff5cc8b4
Hamideh Jalilpour <HamidehJalilpour@outlook.com>
2025-09-27 16:15:01 +0330

    feat(api): add minimal API and DI registration

--- Shipping.Api/Program.cs
+++ Shipping.Api/Program.cs
@@ -1,8 +1,16 @@
+using Shipping.Application.DTOs;
+using Shipping.Application.UseCases;
+using Shipping.Core.Interfaces;
+using Shipping.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IOrderRepository,InMemoryOrderRepository>();
+builder.Services.AddScoped<ICreateOrderUseCase, CreateOrderUseCase>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,5 +29,17 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapPost("/orders", async (CreateOrderRequest request, ICreateOrderUseCase uc) =>
+{
+    var id = await uc.ExecuteAsync(request);
+    return Results.Created($"/orders/{id}", new { id });
+});
+
+app.MapGet("/orders/{id:guid}", async (Guid id, IOrderRepository repo) =>
+{
+    var order = await repo.GetByIdAsync(id);
+    return order is null ? Results.NotFound() : Results.Ok(order);
+});
 
 app.Run();

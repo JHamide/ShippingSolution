fba353fb9fc5d4cd7af6cdff8f7c13e615ab9a65
Hamideh Jalilpour <HamidehJalilpour@outlook.com>
2025-09-27 15:04:20 +0330

    feat(infra): add InMemoryOrderRepository

---
+++ Shipping.Infrastructure/Repositories/InMemoryOrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using Shipping.Core.Entities;
+using Shipping.Core.Interfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Infrastructure.Repositories
+{
+    public class InMemoryOrderRepository : IOrderRepository
+    {
+        private readonly ConcurrentDictionary<Guid, Order> _store = new();
+
+        public Task<Order?> GetByIdAsync(Guid id) =>
+            Task.FromResult(_store.TryGetValue(id, out var o) ? o : null);
+
+        public Task SaveAsync(Order order)
+        {
+            _store[order.Id] = order;
+            return Task.CompletedTask;
+        }
+    }
+}

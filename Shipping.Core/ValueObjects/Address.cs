9fc9f69bc21cc62d5dbad7211fd7ef597fddc4f8
Hamideh Jalilpour <HamidehJalilpour@outlook.com>
2025-09-28 10:39:18 +0330

    feat(core): add address value object

---
+++ Shipping.Core/ValueObjects/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Core.ValueObjects
+{
+    public record Address(string street, string city, string country)
+    {
+        public override string ToString() => $"{street}, {city}, {country}";
+    }
+}

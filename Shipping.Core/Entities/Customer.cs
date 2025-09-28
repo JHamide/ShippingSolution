f6b6a977403ecffe4b1f87f17f58b5a0967259bc
Hamideh Jalilpour <HamidehJalilpour@outlook.com>
2025-09-28 11:06:11 +0330

    refactor(core): update Customer entity with validation in new Customer and add  empty costructor

--- Shipping.Core/Entities/Customer.cs
+++ Shipping.Core/Entities/Customer.cs
@@ -6,12 +6,27 @@ using System.Threading.Tasks;
 
 namespace Shipping.Core.Entities
 {
-    public class Customer(Guid? id = null)
+    public class Customer
     {
-        public Guid Id { get; private set; } = id ?? Guid.NewGuid();
+        public Guid Id { get; private set; } = Guid.NewGuid();
 
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; private set; }
 
-        public string Email { get; set; } = string.Empty;
+        public string Email { get; private set; }
+
+        private Customer() { }
+
+        public Customer(string name, string email)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name cannot be empty", nameof(name));
+
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("Email cannot be empty", nameof(email));
+
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+        }
     }
 }

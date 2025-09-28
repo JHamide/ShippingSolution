c8a8db535caa751de959d64b3765a050310c6e22
Hamideh Jalilpour <HamidehJalilpour@outlook.com>
2025-09-28 10:40:18 +0330

    feat(core, infra): implement InMemoryCustomerRepository

---
+++ Shipping.Core/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Shipping.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Core.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer?> GetByIdAsync(Guid id);
+
+        Task SaveAsync(Customer customer);
+    }
+}
---
+++ Shipping.Infrastructure/Repositories/InMemoryCustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using Shipping.Core.Entities;
+using Shipping.Core.Interfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Infrastructure.Repositories
+{
+    public class InMemoryCustomerRepository : ICustomerRepository
+    {
+        private readonly ConcurrentDictionary<Guid, Customer> _store = new();
+
+        public Task<Customer?> GetByIdAsync(Guid id) => Task.FromResult(_store.TryGetValue(id, out var customer) ? customer : null);
+
+        public Task SaveAsync(Customer customer)
+        {
+            _store[customer.Id] = customer;
+            return Task.CompletedTask;
+        }
+    }
+}

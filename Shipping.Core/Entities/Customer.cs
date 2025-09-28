f50849ccb97ea8acb2ead145c24457e74d5b7861
Hamideh Jalilpour <HamidehJalilpour@outlook.com>
2025-09-28 10:37:54 +0330

    feat(core|): add customer entity

---
+++ Shipping.Core/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Core.Entities
+{
+    public class Customer(Guid? id = null)
+    {
+        public Guid Id { get; private set; } = id ?? Guid.NewGuid();
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+    }
+}

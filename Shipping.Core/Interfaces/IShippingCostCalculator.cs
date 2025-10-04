4f5f555622427d55819161a1d7b7f1eedf3ad795
Hamideh Jalilpour <HamidehJalilpour@outlook.com>
2025-10-04 09:35:12 +0330

    feat(core): add IshippingCostCalculator interface

---
+++ Shipping.Core/Interfaces/IShippingCostCalculator.cs
@@ -0,0 +1,15 @@
+﻿using Shipping.Core.Entities;
+using Shipping.Core.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Core.Interfaces
+{
+    public interface IShippingCostCalculator
+    {
+        Money CalculateShippingCost(Order order);
+    }
+}

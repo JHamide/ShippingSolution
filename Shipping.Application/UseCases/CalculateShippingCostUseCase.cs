97d05737b24b0296576605337a62e9f52b54848c
Hamideh Jalilpour <HamidehJalilpour@outlook.com>
2025-10-04 09:56:51 +0330

    feat(application): add CalculateShippingCostUseCase

---
+++ Shipping.Application/UseCases/CalculateShippingCostUseCase.cs
@@ -0,0 +1,27 @@
+﻿using Shipping.Core.Entities;
+using Shipping.Core.Interfaces;
+using Shipping.Core.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Application.UseCases
+{
+    internal class CalculateShippingCostUseCase
+    {
+        private readonly IShippingCostCalculator _calculator;
+
+        public CalculateShippingCostUseCase(IShippingCostCalculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        public Money Execute(Order order)
+        {
+            if (order == null) throw new ArgumentNullException(nameof(order));
+            return _calculator.CalculateShippingCost(order);
+        }
+    }
+}

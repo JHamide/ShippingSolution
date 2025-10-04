3125cf56114649d1c2ed85cff0b4fa8d9147db4e
Hamideh Jalilpour <HamidehJalilpour@outlook.com>
2025-10-04 09:51:46 +0330

    feat(infra): add FlatRateShippingCalculator and PercentageShippingCalculator

---
+++ Shipping.Infrastructure/Services/FlatRateShippingCalculator.cs
@@ -0,0 +1,23 @@
+﻿using Shipping.Core.Entities;
+using Shipping.Core.Interfaces;
+using Shipping.Core.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Infrastructure.Services
+{
+    public class FlatRateShippingCalculator : IShippingCostCalculator
+    {
+        private readonly Money _flatRate;
+
+        public FlatRateShippingCalculator()
+        {
+            _flatRate = new Money(10m, "USD");
+        }
+
+        public Money CalculateShippingCost(Order order) => _flatRate;
+    }
+}
---
+++ Shipping.Infrastructure/Services/PercentageShippingCalculator.cs
@@ -0,0 +1,29 @@
+﻿using Shipping.Core.Entities;
+using Shipping.Core.Interfaces;
+using Shipping.Core.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Infrastructure.Services
+{
+    internal class PercentageShippingCalculator : IShippingCostCalculator
+    {
+        private readonly decimal _rate;
+
+        public PercentageShippingCalculator(decimal rate)
+        {
+            if (rate < 0) throw new ArgumentException("rate must be >= 0", nameof(rate));
+            _rate = rate;
+        }
+
+        public Money CalculateShippingCost(Order order)
+        {
+            var total = order.Total();
+            var cost = Math.Round(total.Amount * _rate, 2);
+            return new Money(cost, total.Currency);
+        }
+    }
+}

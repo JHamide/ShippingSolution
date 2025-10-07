d0572da5812fa492203ae21b182d9ee119e5866f
Hamideh Jalilpour <HamidehJalilpour@outlook.com>
2025-10-07 11:24:45 +0330

    feat(infra): add SqlOrderRepository using ShippingDbContext (EF Core)

---
+++ Shipping.Infrastructure/Persistence/SqlOrderRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shipping.Core.Entities;
+using Shipping.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Infrastructure.Persistence
+{
+    internal class SqlOrderRepository : IOrderRepository
+    {
+        private readonly ShippingDbContext _db;
+        public SqlOrderRepository(ShippingDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddAsync(Order order)
+        {
+            _db.Orders.Add(order);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<Order?> GetByIdAsync(Guid id)
+        {
+            return await _db.Orders
+                .Include("Lines") // include owned collection
+                .FirstOrDefaultAsync(o => o.Id == id);
+        }
+
+        public async Task<IEnumerable<Order>> GetAllAsync()
+        {
+            return await _db.Orders
+                .Include("Lines")
+                .ToListAsync();
+        }
+
+        public Task SaveAsync(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
